66cda1b65d36b801509ca44c8b76e123704fb6bb
Huy Tú <you@example.com>
2021-08-27 10:17:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Xin chào Đặng Huy Tú
+    </div>
+  );
+}
+
+export default App;
